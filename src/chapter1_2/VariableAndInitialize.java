9484123120b760d9d10a7e48cb1c56f645e21c99
sinbum <sinbum@kakao.com>
2024-06-25 15:39:22 +0900

    변수 선언과 초기화

---
+++ src/chapter1_2/VariableAndInitialize.java
@@ -0,0 +1,18 @@
+package chapter1_2;
+
+/*
+변수 선언과 초기화
+*/
+
+public class VariableAndInitialize {
+    public static void main(String[] args) {
+        // 지시사항 1번을 참고하여 코드를 작성하세요.
+        int number = 100;
+        double first_number = 1.1;
+        char alphabet = 'A';
+        // 지시사항 2번을 참고하여 코드를 작성하세요.
+        System.out.println(number);
+        System.out.println(first_number);
+        System.out.println(alphabet);
+    }
+}
