80abf8cf623089e7b2d80151770f7fd134a8660b
[코치] 신범 <sinbum@kakao.com>
2024-06-27 17:19:11 +0900

    1-4  약수 구하기

---
+++ src/chapter1_4/EvenNumber.java
@@ -0,0 +1,20 @@
+package chapter1_4;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        // 아래 코드는 문제 해결을 위해 기본적으로 제공되는 코드입니다. 수정하지 마세요!
+        int[] iArray = new int[] {
+                1, 5, 3, 43, 31, 65, 8865, 243, 7743, 13245, 9541, 375, 142, 3645, 8357, 2734, 6845, 184835, 2, 1
+        };
+
+        for (int i : iArray) {
+            // 지시사항을 참고하여 코드를 작성하세요.
+
+            if (i % 2 == 0) {
+                System.out.println(i);
+                break;
+            }
+
+        }
+    }
+}
