b7f94dadac711ca80506d737938fc73ff3751dda
[코치] 신범 <sinbum@kakao.com>
2024-06-27 17:18:43 +0900

    1-4  구구단

---
+++ src/chapter1_4/TimesTable.java
@@ -0,0 +1,19 @@
+package chapter1_4;
+
+import java.util.Scanner;
+
+// 구구단 n단 출력하기
+public class TimesTable {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        // 지시사항 1번을 참고하여 코드를 작성하세요.
+        for(int i = 1; i <= 9; i++)
+        {
+            System.out.println(n * i);
+        }
+
+    }
+}
