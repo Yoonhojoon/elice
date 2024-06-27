7fc43274a13f49117f7d335c037da7885bb3c0d1
[코치] 신범 <sinbum@kakao.com>
2024-06-27 17:19:39 +0900

    1-4  배열의 생성과 출력

---
+++ src/chapter1_4/ArrayCreate.java
@@ -0,0 +1,18 @@
+package chapter1_4;
+
+// 배열 생성과 출력
+public class ArrayCreate {
+    public static void main(String args[]) {
+        // 지시사항 1번을 참고하여 코드를 작성하세요.
+        int[] iArray = new int[3];
+
+        // 지시사항 2번을 참고하여 코드를 작성하세요.
+        iArray[0] = 1;
+        iArray[1] = 2;
+        iArray[2] = 3;
+
+        System.out.println("iArray[0] : " + iArray[0]);
+        System.out.println("iArray[1] : " + iArray[1]);
+        System.out.println("iArray[2] : " + iArray[2]);
+    }
+}
