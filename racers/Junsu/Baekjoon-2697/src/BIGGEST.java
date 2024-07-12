89fe4dcefd59df33cb6f3a36693ff8b1e7efc104
Sur0n <skyzephyr333@gmail.com>
2024-07-12 14:17:30 +0900

    add Algorithm 2697 directory

---
+++ racers/Junsu/Baekjoon-2697/src/BIGGEST.java
@@ -0,0 +1,38 @@
+package elice;
+
+class NetworkException extends Exception {
+    public NetworkException(String message) {
+        super(message);
+    }
+}
+
+class AccountInfoException extends Exception {
+    public AccountInfoException(String message) {
+        super(message);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 지시 사항을 참고하여 코드를 작성해 보세요.
+        checkBalance(Math.random());
+
+    }
+
+    static void checkBalance(double randomValue) throws NetworkException, AccountInfoException {
+
+        // 지시 사항을 참고하여 코드를 작성해 보세요.
+        try{
+            if(randomValue <= 0.5){
+                throw new AccountInfoException("계좌 정보를 찾을 수 없습니다.");
+            }
+            throw new NetworkException("네트워크 연결 오류가 발생했습니다.");
+        }catch (NetworkException e){
+            e.getMessage();
+        }catch (AccountInfoException e){
+            e.getMessage();
+        }
+
+    }
+}
