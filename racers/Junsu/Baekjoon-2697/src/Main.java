df25f6a5a5ae401a033b3a683771f6ed78290519
Sur0n <skyzephyr333@gmail.com>
2024-07-13 02:35:05 +0900

    change error Test

--- racers/Junsu/Baekjoon-2697/src/Main.java
+++ racers/Junsu/Baekjoon-2697/src/Main.java
@@ -19,9 +19,9 @@ public class Main {
         try {
             checkBalance(Math.random());
         } catch (NetworkException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         } catch (AccountInfoException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         }
 
     }
@@ -29,16 +29,10 @@ public class Main {
     static void checkBalance(double randomValue) throws NetworkException, AccountInfoException {
 
         // 지시 사항을 참고하여 코드를 작성해 보세요.
-        try{
-            if(randomValue <= 0.5){
-                throw new AccountInfoException("계좌 정보를 찾을 수 없습니다.");
-            }
-            throw new NetworkException("네트워크 연결 오류가 발생했습니다.");
-        }catch (NetworkException e){
-            e.getMessage();
-        }catch (AccountInfoException e){
-            e.getMessage();
+        if(randomValue <= 0.5){
+            throw new AccountInfoException("계좌 정보를 찾을 수 없습니다.");
         }
+        throw new NetworkException("네트워크 연결 오류가 발생했습니다.");
 
     }
 }
