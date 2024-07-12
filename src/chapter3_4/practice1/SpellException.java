3ac6f2fa7944fdbab6a1a09557f40821e51a1ab9
sinbum <sinbum@kakao.com>
2024-07-12 15:59:53 +0900

    3-4-practice1 마법사의 주문

---
+++ src/chapter3_4/practice1/SpellException.java
@@ -0,0 +1,8 @@
+package chapter3_4.practice1;
+
+// 지시 사항을 참고하여 코드를 작성해 보세요.
+class SpellException  extends Exception{
+    public SpellException(String message) {
+        super(message);
+    }
+}
